996be67ed0e7a9db12dea78699c1bb951d97f4be
Eigene Datei <waldemar65@hotmail.de>
2026-03-31 17:50:17 +0200

    C:\Users\Waldemar Orschlet\source\repos\McAfee Add-On Modules\Program.cs###

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Emit;
+
+namespace McAfee_Add_On_Modules
+{
+    internal class Program  48b1da5ec82833eb63a4bc1db148b9ee0c8509a2
+                STATS TABLE CALC /HELP.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
