a27ae0cf243193f486a2794126fca8d7643f333d
Eigene Datei <waldemar65@hotmail.de>
2026-04-09 16:28:55 +0200

    #

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System.Reflection.Emit;
 
 namespace McAfee_Add_On_Modules
 {
     internal class Program  48b1da5ec82833eb63a4bc1db148b9ee0c8509a2
                 STATS TABLE CALC /HELP.
+=======
+﻿namespace McAfee_Add_On_Modules
+{
+    internal class Program  GlobalUsing
+>>>>>>> ea39fe7778a64b440f7808756db41569cb6d0b99
 =======
 ﻿namespace McAfee_Add_On_Modules
 {
